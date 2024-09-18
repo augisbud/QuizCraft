9d1108850472a54ef1ee1ab0ff90122015783f69
Augustas Budnikas <budnikiukas@gmail.com>
2024-09-18 19:46:37 +0300

    Add Configuration for IntegrationTests

--- QuizCraft.Domain.API.Tests/QuizCraft.Domain.API.Tests.Integration/CustomWebApplicationFactory.cs
+++ QuizCraft.Domain.API.Tests/QuizCraft.Domain.API.Tests.Integration/CustomWebApplicationFactory.cs
@@ -1,6 +1,7 @@
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc.Testing;
 using Microsoft.AspNetCore.TestHost;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using QuizCraft.Domain.API.APIClients;
 
@@ -11,6 +12,14 @@ public class CustomWebApplicationFactory : WebApplicationFactory<Program>
     protected override void ConfigureWebHost(IWebHostBuilder builder)
     {
         builder.UseEnvironment("Testing");
+
+        builder.UseConfiguration(
+            new ConfigurationBuilder()
+                .AddInMemoryCollection([
+                    new KeyValuePair<string, string?>("GeminiAPIKey", "fake-api-key")
+                ])
+                .Build()
+        );
         
         builder.ConfigureTestServices(services =>
         {
